5617918fcb6d423c1222dd5cd43c8270a554002b
ydawit87 <yosephd627@gmail.com>
2023-06-24 14:23:57 -0700

    edits to app file to run flask app

--- app.py
+++ app.py
@@ -1,64 +1,17 @@
-from flask import Flask, render_template, request, jsonify
-from azure.cosmos import CosmosClient, PartitionKey
-import openai
+from flask import Flask
+from config import Config
+from services import CosmosDbService, OpenAIService
 
-# Initialize Flask app
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# Initialize Cosmos DB client
-endpoint = "<your_cosmos_db_endpoint>"
-key = '<your_cosmos_db_key>'
-database_name = '<your_database_name>'
-container_name = '<your_container_name>'
-client = CosmosClient(endpoint, key)
-container = client.get_database_client(database_name).get_container_client(container_name)
+cosmos_service = None
+if app.config.get('COSMOS_DB_ENDPOINT') and app.config.get('COSMOS_DB_KEY'):
+    cosmos_service = CosmosDbService(app.config['COSMOS_DB_ENDPOINT'], app.config['COSMOS_DB_KEY'], app.config['COSMOS_DB_DATABASE_NAME'], app.config['COSMOS_DB_CONTAINER_NAME'])
 
-# Initialize OpenAI GPT-3
-openai.api_key = '<your_openai_api_key>'
+openai_service = None
+if app.config.get('OPENAI_API_KEY'):
+    openai_service = OpenAIService(app.config['OPENAI_API_KEY'])
 
-
-def process_message_with_gpt3(message):
-    # Use the OpenAI API to generate a response
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=message,
-        temperature=0.5,
-        max_tokens=100
-    )
-
-    # Return the response
-    return response.choices[0].text.strip()
-
-
-def store_message_and_response_in_db(message, response):
-    # Create a document with the message and response
-    document = {
-        'id': str(uuid.uuid4()),
-        'message': message,
-        'response': response
-    }
-
-    # Add the document to the Cosmos DB container
-    container.upsert_item(document)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/message', methods=['POST'])
-def message():
-    # Get message from request
-    message = request.json['message']
-
-    # TODO: Process message and generate response using GPT-3
-    response = process_message_with_gpt3(message)
-
-    # TODO: Store message and response in Cosmos DB
-    store_message_and_response_in_db(message, response)
-
-    # Return response
-    return jsonify({'response': response})
-
-if __name__ == '__main__':
-    app.run(debug=True)
+import routes
